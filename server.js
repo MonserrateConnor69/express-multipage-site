2d0c8f89df03ab057267de77e5bf2135e8e5b86f
MonserrateConnor69 <mrgobasco@tip.edu.ph>
2026-02-17 00:04:55 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -7,34 +7,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
-
-app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
-});
-
-app.get('/blog', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
-});
+// Task 2: Blog Routes
+app.get('/blog', (req, res) => res.sendFile(path.join(__dirname, 'views', 'blog.html')));
 
 app.get('/api/posts', (req, res) => {
     fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Error reading data');
-            return;
-        }
+        if (err) return res.status(500).send('Error');
         res.json(JSON.parse(data));
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
